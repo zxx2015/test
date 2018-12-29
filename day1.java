162cd4357df32cd4360e45fc76b4bde288ba8e71
zxx2015 <1070976973@qq.com>
2018-12-29 11:23:52 +0800

    day1

---
+++ day1.java
@@ -0,0 +1,31 @@
+public class day1 {
+
+    public static int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) {
+            return -1;
+        }
+        int i = 0;
+        int j = 0;
+        for (; i < haystack.length() && j < needle.length(); ++i) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+                if (j == needle.length() - 1 && haystack.charAt(i) == needle.charAt(j))
+
+                    break;
+            } else {
+                j = 0;
+            }
+        }ggi
+        if(j==needle.length()-1) {
+            return i - needle.length() + 1;
+        }
+        else return-1;
+
+    }
+
+    public static void main(String args[]){
+
+        System.out.println(strStr("hello", "llo" ));
+    }
+
+}
