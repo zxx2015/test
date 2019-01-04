c641aab0e7c5d6a6f1144229532798e10486cb25
zxx2015 <1070976973@qq.com>
2019-01-04 15:31:56 +0800

    day4

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
+
+
+        /*----------------------day2---------------------
         day2 day2 = new day2();
         int res = day2.delRepeat(new int[]{1, 1, 2, 2, 3} );
         System.out.println(res);
+        */
+
+        /*----------------------day3---------------------
+        day3 day3 = new day3();
+        int res1 = day3.maxArea(new int[]{1,8,6,2,5,4,8,3,7});
+        System.out.println(res1);*/
+
+
+        /*----------------------day4---------------------*/
+        day4 day4 = new day4();
+        boolean res4 = day4.isPalindrome(".,");
+        System.out.println(res4);
+
+
+
+
     }
 }
